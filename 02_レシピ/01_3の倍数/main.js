4cff0b8209444cd7818f201a9a81180c51302761
nobuoooo <marshmellogarrix600@gmail.com>
2022-04-05 20:15:10 +0900

    FizzBuzzと3が大好き

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -1,11 +1,40 @@
-const genkiFunction = function (number) {
+// const genkiFunction = function (number) {
+//   for (let n = 1; n < number; n++) {
+//     if (n % 3 === 0) {
+//       console.log(n + "!!!!")
+//     } else {
+//       console.log(n)
+//     }
+//   }
+// }
+
+// genkiFunction(50)
+
+// FizzBuzz
+// const FizzBuzz = function (number) {
+//   for (let n = 1; n < number; n++) {
+//     if (n % 15 === 0) {
+//       console.log("FizzBuzz")
+//     } else if (n % 5 === 0) {
+//       console.log("Buzz")
+//     } else if (n % 3 === 0) {
+//       console.log("Fizz")
+//     } else {
+//       console.log(n)
+//     }
+//   }
+// }
+
+// FizzBuzz(50)
+
+// 3が大好き
+const love3 = function (number) {
   for (let n = 1; n < number; n++) {
-    if (n % 3 === 0) {
+    if (String(n).indexOf("3") !== -1) {
       console.log(n + "!!!!")
     } else {
       console.log(n)
     }
   }
 }
-
-genkiFunction(50)
+love3(50)
